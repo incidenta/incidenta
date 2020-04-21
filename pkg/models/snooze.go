c2c321ddc7c6ad07364e77898f2c8f71dd3693c9
l.aminov <l.aminov@tinkoff.ru>
2020-04-21 18:43:33 +0500

    Adds GetSnoozeByAlertFingerprint

--- pkg/models/snooze.go
+++ pkg/models/snooze.go
@@ -1,8 +1,11 @@
 package models
 
 import (
+	"fmt"
+
 	apiv1 "github.com/incidenta/incidenta/pkg/api/v1"
 	"github.com/incidenta/incidenta/pkg/timeutil"
+	"xorm.io/xorm"
 )
 
 type Snooze struct {
@@ -26,4 +29,36 @@ func (s *Snooze) APIFormat() *apiv1.Snooze {
 		CreatedUnix:  s.CreatedUnix.AsTime(),
 		UpdatedUnix:  s.UpdatedUnix.AsTime(),
 	}
+}
+
+func GetSnoozeByAlertFingerprint(fingerprint string) (*Snooze, error) {
+	return getSnoozeByAlertFingerprint(x, fingerprint)
+}
+
+type ErrSnoozeNotExist struct {
+	ID          int64
+	Fingerprint string
+}
+
+func IsErrSnoozeNotExist(err error) bool {
+	_, ok := err.(ErrSnoozeNotExist)
+	return ok
+}
+
+func (e ErrSnoozeNotExist) Error() string {
+	return fmt.Sprintf("Snooze does not exist [id: %d, fingerprint: %s]", e.ID, e.Fingerprint)
+}
+
+func getSnoozeByAlertFingerprint(x *xorm.Engine, fingerprint string) (*Snooze, error) {
+	if len(fingerprint) == 0 {
+		return nil, ErrSnoozeNotExist{Fingerprint: fingerprint}
+	}
+	s := &Snooze{AlertFingerprint: fingerprint}
+	has, err := x.Get(s)
+	if err != nil {
+		return nil, err
+	} else if !has {
+		return nil, ErrSnoozeNotExist{Fingerprint: fingerprint}
+	}
+	return s, nil
 }
